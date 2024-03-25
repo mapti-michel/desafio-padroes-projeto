bc313cb5f99a88b46c953956c552b6275800de76
Michel Pereira <pereira.michel@gmail.com>
2024-03-24 22:12:09 -0300

    Inicio do desafio

---
+++ src/main/java/br/labdio/DesafioPadroesProjetoApplication.java
@@ -0,0 +1,13 @@
+package br.labdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPadroesProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPadroesProjetoApplication.class, args);
+	}
+
+}
